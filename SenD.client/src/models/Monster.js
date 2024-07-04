5f874923487b84d7368d4f58cb8054cb83212b0b
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-07-04 02:57:47 -0600

    Created monster table in database

--- SenD.client/src/models/Monster.js
+++ SenD.client/src/models/Monster.js
@@ -3,16 +3,12 @@ export class Monster {
     this.id = data.id
     this.name = data.name
     this.img = data.img
-    this.stats = data.stats
     this.creator = data.creator
     this.description = data.description
-    this.monsterMessageId = data.monsterMessageId
-    // {
-    //   hp = data.stats.hp
-    //   type = data.stats.type
-    //   subType = data.stats.subType
-    //   power = data.stats.power
-    //   toughness = data.stats.power
-    // }
+    // this.monsterMessageId = data.monsterMessageId
+    this.type = data.stats.type
+    this.subType = data.stats.subType
+    this.power = data.stats.power
+    this.toughness = data.stats.power
   }
 }
--- SenD/dbSetup.sql
+++ SenD/dbSetup.sql
@@ -8,3 +8,18 @@ CREATE TABLE IF NOT EXISTS accounts(
   picture varchar(255) COMMENT 'User Picture',
   coverImg varchar(700)
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE monsters(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name varchar(255) NOT NULL COMMENT 'Monster Name',
+  type varchar(255) NOT NULL COMMENT 'Monster Type',
+  subType varchar(255) COMMENT 'Monster Sub-Type',
+  img varchar(100) NOT NULL COMMENT 'Monster Image',
+  description varchar(1000) NOT NULL COMMENT 'Monster Description',
+  power INT NOT NULL COMMENT 'Monster Power',
+  toughness INT NOT NULL COMMENT 'Monster Toughness',
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY(creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
